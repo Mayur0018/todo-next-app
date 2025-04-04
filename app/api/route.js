85788c330ead3e8f3a5de8cfff7dd6ca6e084b6d
Mayur0018 <mayurnish18@gmail.com>
2025-04-04 23:31:46 +0530

    coomit

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 import { ConnectDB } from "@/lib/confing/db";
-
+import TodoModel from "@/lib/models/TodoModels";
 const loaddb = async () => {
   await ConnectDB();
 };
 loaddb();
 export async function GET() {
+  return NextResponse.json({ msg: "Get Method Hit" });
+}
+export async function POST(request) {
+  const { titile, description } = await request.join();
+  await TodoModel.create({
+    titile,
+    description,
+  });
   return NextResponse.json({ msg: "Get Method Hit" });
 }
