faf3067a3bfb9617de997b46216582d70c15e270
Mayur0018 <mayurnish18@gmail.com>
2025-04-04 23:04:20 +0530

    commit

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,10 @@
 import { NextResponse } from "next/server";
-export async function GET(){
-   return NextResponse.json({msg: "Get Method Hit"});
-}   
+import { ConnectDB } from "@/lib/confing/db";
+
+const loaddb = async () => {
+  await ConnectDB();
+};
+loaddb();
+export async function GET() {
+  return NextResponse.json({ msg: "Get Method Hit" });
+}
